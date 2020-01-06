1f308e40a25e93dbce30dbf4b62000090bf280aa
716green <bob@bbass.co>
2020-01-05 23:41:47 -0500

    Removed Linting to enable server running, just installed express. run client and server

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,11 @@
-// Within client, run: "npm run dev" to start the server
+// WAS:
+    // "start": "./node_modules/nodemon/bin/nodemon.js src/app.js --exec 'npm run lint && node'",
+    // "lint": "./node_modules/.bin/eslint */**.js"
+// NOW (Removed Linting):
+  // "start": "./node_modules/nodemon/bin/nodemon.js src/app.js --exec 'npm run && node'",
+
+// start client and server (do this in 2 separate terminal instances):
+// // /client/$ npm run dev
+// // /server/$ npm start
 
 console.log('hello');
